8a3468e552aba5929c5eed1983cec8ee756dd29b
Zhiyuan Liu <cloudnotes.64@gmail.com>
2020-03-04 18:39:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,18 +2,15 @@
 def hopper
 	programmer_hash = 
  		{
-        :grace_hopper => 
-        {
+        :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
-        :alan_kay => 
-        {
+        :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
-        :dennis_ritchie => 
-        {
+        :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
         }
@@ -23,8 +20,8 @@ def hopper
 end
 
 def alan_kay_is_known_for
-  hopper[:alan_kay][:known_for]
-end
+	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	
 	programmer_hash = 
  		{
         :grace_hopper => {
