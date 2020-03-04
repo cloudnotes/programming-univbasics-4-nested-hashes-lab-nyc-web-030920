62898d840f5d522393cadaafecccc0a2fe5430c2
Zhiyuan Liu <cloudnotes.64@gmail.com>
2020-03-04 18:53:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -99,7 +99,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
@@ -116,7 +117,7 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => ["C", "Assembly"]
         }
      }
 
