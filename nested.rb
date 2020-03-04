ef6fa190edbc72276b6900b1d31ed57fccdc068f
Zhiyuan Liu <cloudnotes.64@gmail.com>
2020-03-04 18:45:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,6 @@ def hopper
         }
      }
 
-
 end
 
 def alan_kay_is_known_for
@@ -55,18 +54,17 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  programmer_hash[]
+  programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash = 
  		{
+         :yukihiro_matsumoto => {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
